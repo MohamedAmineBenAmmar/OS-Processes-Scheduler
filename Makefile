a9942f4603f78b016d8858787c113afe2dd66f05
Mohamed Amine Ben Ammar <ma.benammar5@gmail.com>
2021-11-15 18:54:15 -0800

    fix issues and preparing for the scheduling policies implementation

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
-
+SCHEDULING_POLICIES_SRC_DIR = scheduling_policies/src
+SCHEDULING_POLICIES_BUILD_DIR = scheduling_policies/build
 
 output: build/*.o
 	gcc build/*.o -o build/output
 	
-# Define objects for all sources
-scheduling_policies_objs := $(subst $(SOURCEDIR),$(BUILDDIR),$(SOURCES:.c=.o))	
-	
 
-	
+
+scheduling_policies_executables: scheduling_policies/build/*.o
+	@for f in $(shell ls -1 *.o ${SCHEDULING_POLICIES_BUILD_DIR} | sed 's/\.[a-z]*//g'); do gcc ${SCHEDULING_POLICIES_BUILD_DIR}/$${f}.o -o ${SCHEDULING_POLICIES_BUILD_DIR}/$${f} ; done
+
 scheduling_policies.o: scheduling_policies/src/*.c
-	gcc -c scheduling_policies/src/*.c && mv ./*.o build 
+	@for f in $(shell ls -1 *.c ${SCHEDULING_POLICIES_SRC_DIR} | sed 's/\.[a-z]*//g'); do gcc -c ${SCHEDULING_POLICIES_SRC_DIR}/$${f}.c -o ${SCHEDULING_POLICIES_BUILD_DIR}/$${f}.o ; done
 
 main.o: main.c
 	gcc -c main.c -o build/main.o
---
+++ scheduling_policies/src/FIFO.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include "../../file_manager_functions.h"
+
+
+int main(int argc, char **argv)
+{
+    /* Test */
+    int n, i;
+    char path[256];
+    char *full_path_with_spargs;
+
+    // Init path variable
+    strcpy(path, "./scheduling_policies/build/PROG");
+
+    // Preparing the args to be passed to the scheduling policy
+    n = 0;
+    for (i = 1; i < argc; i++)
+    {
+        n++;
+        n += strlen(argv[i]);
+    }
+
+    full_path_with_spargs = (char *)malloc((n + strlen(path)) * sizeof(char));
+    if (full_path_with_spargs == NULL)
+    {
+        printf("Memory not allocated.\n");
+        return -1;
+    }
+    else
+    {
+        strcpy(full_path_with_spargs, path);
+        for (i = 1; i < argc; i++)
+        {
+            strcat(full_path_with_spargs, " ");
+            strcat(full_path_with_spargs, argv[i]);
+        }
+
+        printf("\nThe value of n is equal to = %d\n", strlen(full_path_with_spargs));
+        puts(full_path_with_spargs);
+        int status = system(full_path_with_spargs);
+    }
+
+return 0;
+}
