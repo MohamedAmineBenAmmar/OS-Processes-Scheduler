ff8d13c0ff51e46f4a71d3e567be55c7b5413c0c
Mohamed Amine Ben Ammar <ma.benammar5@gmail.com>
2021-12-11 16:13:36 -0800

    finish the dynamic priority algorithm

--- scheduling_policies/src/Dynamic_priority.c
+++ scheduling_policies/src/Dynamic_priority.c
@@ -2,15 +2,20 @@
 #include "../../file_manager/file_manager_functions.h"
 #include "shared/priority_functions.h"
 
-
-void dynamic_priority(PL pl, char *priority){
+void dynamic_priority(PL pl, char *priority)
+{
     ReadyQueue rq;
     PLNode *ptr;
+    PLNode *ptrxp;
+    PLNode *process_to_be_selected;
     PLNode *current_running_process;
     int process_arrival_time;
-    int n;
+    int n, search;
 
     int timer;
+    int expected_end_execution_time;
+
+    search = 1;
 
     // Init the ready queue
     rq.head = NULL;
@@ -19,18 +24,20 @@ void dynamic_priority(PL pl, char *priority){
     // Init the pointer used to loop through the linked list
     ptr = pl;
 
-    // Init the ready queue by the elemnts wich will be used later     
+    // Init the ready queue by the elemnts wich will be used later
     enqueue(&rq, ptr);
     timer = ptr->pd.arrival_time;
 
-    
     ptr = ptr->next;
     while (ptr != NULL)
     {
-        if (ptr->pd.arrival_time == timer){
+        if (ptr->pd.arrival_time == timer)
+        {
             enqueue(&rq, ptr);
             ptr = ptr->next;
-        } else {
+        }
+        else
+        {
             break;
         }
     }
@@ -38,60 +45,124 @@ void dynamic_priority(PL pl, char *priority){
     n = 0;
     while (!(isEmptyQueue(rq) == 1))
     {
-        current_running_process = get_process_with_priority(&rq, priority);
+        if (search == 1)
+        {
+            current_running_process = get_process_with_priority(&rq, priority);
+        }
+        else
+        {
+            current_running_process = process_to_be_selected;
+        }
+
         // The current running process is going to run for a certain amount of time
         // until a process with more priority come and take his palace
+        expected_end_execution_time = current_running_process->pd.duration + timer;
 
-
-
-        if (current_running_process->pd.arrival_time > timer){
-            printf("The CPU was empty from %d to %d\n", timer, current_running_process->pd.arrival_time);
-            timer = current_running_process->pd.arrival_time;
-        } else if (timer >= current_running_process->pd.arrival_time && n == 1) {
-            printf("The process: %s was in the ready queue from the moment he wanted the CPU at %d to %d the moment when the CPU is empty\n", current_running_process->pd.process_name,current_running_process->pd.arrival_time, timer);
-        }
-/*
-        (n == 0) ?
-        (printf("The process: %s ran from %d with a duration of %d and left the cpu at %d\n", current_running_process->pd.process_name, current_running_process->pd.arrival_time, current_running_process->pd.duration, current_running_process->pd.arrival_time + current_running_process->pd.duration))
-        :
-        (printf("The process: %s ran from %d with a duration of %d and left the cpu at %d\n", current_running_process->pd.process_name, timer, current_running_process->pd.duration, timer + current_running_process->pd.duration));
-*/
-        printf("The process: %s ran from %d with a priority equal to %d and left the cpu at %d\n", current_running_process->pd.process_name, timer, current_running_process->pd.priority, timer + current_running_process->pd.duration);        
-        timer+= current_running_process->pd.duration;
-
+        //
+        search = 1;
+        process_to_be_selected = NULL;
         while (ptr != NULL)
         {
-            if (ptr->pd.arrival_time <= timer){
-                enqueue(&rq, ptr);
-                ptr = ptr->next;
-            } else {
-                // Test if the ready queue is empty
-                if (isEmptyQueue(rq) == 1) {
+            if (ptr->pd.arrival_time < expected_end_execution_time)
+            {
+                if (ptr->pd.priority <= current_running_process->pd.priority)
+                {
                     enqueue(&rq, ptr);
-                    ptr = ptr->next;
                 }
-                break;
+                else
+                {
+                    // Find the process with the highest priority:
+                    // @time < expected end execution time
+                    // priority > priority of the current running process
+                    search = 0;
+                    process_to_be_selected = ptr;
+                    ptrxp = ptr->next;
+                    while (ptrxp != NULL)
+                    {
+                        if (ptrxp->pd.priority > process_to_be_selected->pd.priority && ptrxp->pd.arrival_time == process_to_be_selected->pd.arrival_time)
+                        {
+                            enqueue(&rq, process_to_be_selected);
+                            process_to_be_selected = ptrxp;
+                            ptrxp = ptrxp->next;
+                        }
+                        else
+                        {
+                            ptr = ptrxp;
+                            break;
+                        }
+                    }
+                    break;
+                }
             }
+            else
+            {
+                if (ptr->pd.arrival_time == expected_end_execution_time)
+                {
+                    enqueue(&rq, ptr);
+                }
+                else
+                {
+                    if (isEmptyQueue(rq) == 1)
+                    {
+                        enqueue(&rq, ptr);
+                        ptr = ptr->next;
+                    }
+                    break;
+                }
+            }
+
+            ptr = ptr->next;
+        }
+
+        //---------------------
+
+        if (current_running_process->pd.arrival_time > timer)
+        {
+            printf("The CPU was empty from %d to %d\n", timer, current_running_process->pd.arrival_time);
+            timer = current_running_process->pd.arrival_time;
+        }
+        else if (timer > current_running_process->pd.arrival_time && n == 1)
+        {
+            printf("The process: %s was in the ready queue from the moment he wanted the CPU at %d to %d the moment when the CPU is empty\n", current_running_process->pd.process_name, current_running_process->pd.arrival_time, timer);
+        }
+
+        if (process_to_be_selected == NULL)
+        {
+            printf("The process: %s ran from %d with a priority equal to %d and left the cpu at %d (process is not interrupted)\n", current_running_process->pd.process_name, timer, current_running_process->pd.priority, expected_end_execution_time);
+            timer+= current_running_process->pd.duration;
+        }
+        else
+        {
+            printf("The process: %s ran from %d with a priority equal to %d and left the cpu at %d (process is interrupted by %s)\n", current_running_process->pd.process_name, timer, current_running_process->pd.priority, process_to_be_selected->pd.arrival_time, process_to_be_selected->pd.process_name);
+
+            // Here we are sure that the current running process didn't finish its duration
+            // Upgrading the current running process data
+
+            // calculating the remaining duration of the current running process
+            current_running_process->pd.duration = expected_end_execution_time - process_to_be_selected->pd.arrival_time;
+
+            // Upgrading the value of the timer
+            timer = process_to_be_selected->pd.arrival_time;
+
+            // Upgrading the new arrival time of the current running process
+            current_running_process->pd.arrival_time = timer;
+
+            // the current running process is going to leave the cpu
+            enqueue(&rq, current_running_process);
         }
 
         (n == 0) && (n++);
         // Display the state of the ready queue
         // ... to do
-
-
     }
-    
-
 }
-
-
 
 int main(int argc, char **argv)
 {
     PL pl;
     char priority[5];
     strcpy(priority, "ASC");
-    
+
     pl = parse_file(argv[1]);
     pl_sort(pl);
     // print_process_list(pl);
