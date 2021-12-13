1fc6c0d12be001cb9be3dc85549a83e2aefd2bd2
Mohamed Amine Ben Ammar <ma.benammar5@gmail.com>
2021-12-12 17:04:26 -0800

    add the ignore comment and blank lines features

--- file_manager/file_manager.c
+++ file_manager/file_manager.c
@@ -1,32 +1,67 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "file_manager_functions.h"
 
-
-
-
-int verify_line(char* line){
+int verify_line(char *line)
+{
     // A line is going to be skipped in the floowing cases
+    // - Line begins with #
+    // - Line is not well formatted
 
+    int res;
+    int ascii;
+
+    res = 1;
+    // Check if the line is a comment or an empty line
+    if (line[0] == '#' || line[0] == '\n')
+    {
+        res = 0;
+    }
+    else
+    {
+        // Check if the last character is a ","or not
+        ascii = *(line + (strlen(line) - 1));
+        if (ascii == 10)
+        {
+            ascii = *(line + (strlen(line) - 2));
+            if (ascii != 44)
+            {
+                *(line + (strlen(line) - 1)) = ',';
+            }
+        }
+        else
+        {
+            if (ascii != 44)
+            {
+                strcat(line, ",");
+            }
+        }
+    }
+
+    return res;
 }
 
-
-ProcessData extract_process_data(char* line){
+ProcessData extract_process_data(char *line)
+{
     char field[50];
     int n = 0;
     ProcessData pd;
 
     for (int i = 0; i < strlen(line); i++)
     {
-        if (line[i] != ',' && line[i] != '\n' && line[i]!= '\0') {
+        if (line[i] != ',' && line[i] != '\n' && line[i] != '\0')
+        {
             strncat(field, &line[i], 1);
-        } else {
+        }
+        else
+        {
             switch (n)
             {
             case 0:
                 // printf("process name\n");
-                strcpy(pd.process_name,  field); 
+                strcpy(pd.process_name, field);
                 break;
 
             case 1:
@@ -45,31 +80,32 @@ ProcessData extract_process_data(char* line){
                 // printf("Priority\n");
                 pd.priority = atoi(field);
                 break;
-            
+
             default:
                 break;
             }
 
             n++;
             field[0] = '\0';
-
         }
-        
     }
 
     // print_process_data(pd);
     return pd;
-    
 }
 
-PL parse_file(char* fileName) {
-    FILE* file = fopen(fileName, "r"); /* should check the result */
+PL parse_file(char *fileName)
+{
+    int line_verification_status;
+
+    FILE *file = fopen(fileName, "r"); /* should check the result */
     char line[256];
 
     ProcessData pd;
     PL pl;
-    
-    if(!file){
+
+    if (!file)
+    {
         printf("\n Unable to open : %s ", fileName);
         return NULL;
     }
@@ -77,24 +113,31 @@ PL parse_file(char* fileName) {
     // Init the process linked list
     pl = NULL;
 
-    while (fgets(line, sizeof(line), file)) {       
+    while (fgets(line, sizeof(line), file))
+    {
         // Skip comments and empty lines
+        line_verification_status = verify_line(line);
+        if (line_verification_status == 0)
+        {
+            continue;
+        }
+
         // Extract the process data
         pd = extract_process_data(line);
         add_process(&pl, pd);
     }
-    
 
     fclose(file);
 
     return pl;
 }
 
-void pl_sort(PL pl){
+void pl_sort(PL pl)
+{
     PL ptri, ptrj, minptr;
-    ProcessData* inter;
+    ProcessData *inter;
 
-    inter = (ProcessData*)malloc(sizeof(ProcessData));
+    inter = (ProcessData *)malloc(sizeof(ProcessData));
     ptri = pl;
 
     while (ptri->next != NULL)
@@ -103,24 +146,25 @@ void pl_sort(PL pl){
         ptrj = ptri->next;
         while (ptrj != NULL)
         {
-            if (minptr->pd.arrival_time > ptrj->pd.arrival_time){
+            if (minptr->pd.arrival_time > ptrj->pd.arrival_time)
+            {
                 minptr = ptrj;
             }
 
             ptrj = ptrj->next;
         }
 
-        if (ptri != minptr){
+        if (ptri != minptr)
+        {
             *inter = ptri->pd;
             ptri->pd = minptr->pd;
             minptr->pd = *inter;
         }
-        
+
         ptri = ptri->next;
     }
 
     free(inter);
-    
 }
 
 /*
